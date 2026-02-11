5e5b08b0553a9c50cd19f0469d08b9b57c14154e
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-02-11 14:01:28 +0000

    defining struct person

---
+++ src/basics/ex013_struct_person.rs
@@ -0,0 +1,18 @@
+// Define a struct named 'Person' with fields 'name' and 'age'
+struct Person {
+    name: String,
+    age: u32,
+}
+
+fn main() {
+    // Create a new instance of 'Person' struct
+    let person1 = Person {
+        // Assign values to 'name' and 'age' fields
+        name: String::from("Salih Yejide"),
+        age: 30,
+    };
+
+    // Access and print the fields of 'person1'
+    println!("Name: {}", person1.name);
+    println!("Age: {}", person1.age);
+}
